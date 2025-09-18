b71f0389925c84f37406983efb3b96b80bd3b586
ravipatelctf <ravipatelctf@gmail.com>
2025-09-18 17:09:05 +0530

    fix:minor fix in auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -38,11 +38,13 @@ router.post("/signup", async (req, res) => {
 // --------------------- user login ----------------------------
 
 const verifyJWT = (req, res, next) => {
-    const token = req.headers["authorization"];
+    const authHeader = req.headers["authorization"];
 
-    if (!token) {
+    if (!authHeader) {
         return res.status(401).json({message: "No token provided."});
     }
+
+    const token = authHeader.split(" ")[1];
 
     try {
         const decodedToken = jwt.verify(token, JWT_SECRET);
